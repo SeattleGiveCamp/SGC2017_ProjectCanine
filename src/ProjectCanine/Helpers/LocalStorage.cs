19c1897de09f7123ef758cda03f62e75e9cf76d3
durkc <chris@durkin-consulting.com>
2017-10-07 15:45:17 -0700

    Newtonsoft fix

--- src/ProjectCanine/Helpers/LocalStorage.cs
+++ src/ProjectCanine/Helpers/LocalStorage.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using PCLStorage;
 
 namespace ProjectCanine.Helpers
 {
     public class LocalStorage
     {
-    }
+		public async Task PCLStorageSample()
+		{
+			IFolder rootFolder = FileSystem.Current.LocalStorage;
+			IFolder folder = await rootFolder.CreateFolderAsync("MySubFolder",
+				CreationCollisionOption.OpenIfExists);
+			IFile file = await folder.CreateFileAsync("answer.txt",
+				CreationCollisionOption.ReplaceExisting);
+			await file.WriteAllTextAsync("42");
+		}
+	}
 }
