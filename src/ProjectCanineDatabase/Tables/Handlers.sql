179e0203067ec55e7ed86d7f77b7a68a8df049f4
Marcel Englmaier <saurutobi@gmail.com>
2017-10-07 09:48:13 -0700

    Added new tables, deleted tests we're not working on

---
+++ src/ProjectCanineDatabase/Tables/Handlers.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Handlers]
+(
+	[Id] INT IDENTITY(1,1) PRIMARY KEY,
+	[Name_First] VARCHAR(100) NOT NULL,
+	[Name_Last] VARCHAR(100) NOT NULL
+)
--- src/ProjectCanineDatabase/Tables/Tests.sql
+++ src/ProjectCanineDatabase/Tables/Tests.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[Tests]
 (
-	[Id] INT IDENTITY(1,1) PRIMARY KEY, 
-	[Name] VARCHAR(50) NOT NULL, 
-	[LastEditedBy] UNIQUEIDENTIFIER NOT NULL, 
+	[Id] INT IDENTITY(1,1) PRIMARY KEY,
+	[ShortName] VARCHAR(30) NOT NULL,
+	[Name] VARCHAR(500) NOT NULL,
+	[TestVersion_DateTime] DATETIME NOT NULL,
+	[LastEditedBy] INT NOT NULL,
 	[LastEditedDate] DATETIME NOT NULL
 )
