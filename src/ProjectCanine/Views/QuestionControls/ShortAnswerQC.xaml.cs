fff61081435263d8a413a5ca9c798cea12edaed0
Geoffrey Liu <geliu@groupon.com>
2017-10-07 16:49:59 -0700

    Question control placeholders

---
+++ src/ProjectCanine/Views/QuestionControls/ShortAnswerQC.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ProjectCanine
+{
+    public partial class ShortAnswerQC : ContentView
+    {
+        public ShortAnswerQC()
+        {
+            InitializeComponent();
+        }
+    }
+}
