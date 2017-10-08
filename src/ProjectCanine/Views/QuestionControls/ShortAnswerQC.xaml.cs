7fb4a4b762ea918eb2d003f4da403f83bbb2b5a1
Geoffrey Liu <geliu@groupon.com>
2017-10-07 20:32:22 -0700

    Add first page test questions

--- src/ProjectCanine/Views/QuestionControls/ShortAnswerQC.xaml.cs
+++ src/ProjectCanine/Views/QuestionControls/ShortAnswerQC.xaml.cs
@@ -6,9 +6,17 @@ namespace ProjectCanine
 {
     public partial class ShortAnswerQC : ContentView
     {
+        public static BindableProperty TitleProperty;
         public ShortAnswerQC()
         {
             InitializeComponent();
+            TitleProperty = BindableProperty.Create("Title", typeof(String), typeof(ShortAnswerQC), "Short answer label");
+            BindingContext = this;
+        }
+
+        public string Title {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
         }
     }
 }
