699fb195877772e546e8c9acc11367d8565926a1
durkc <chris@durkin-consulting.com>
2017-10-07 18:42:19 -0700

    fall back to local storage in cloud store

--- src/ProjectCanine/Helpers/LocalStorage.cs
+++ src/ProjectCanine/Helpers/LocalStorage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -29,11 +30,39 @@ namespace ProjectCanine.Helpers
 
 		public async Task<T> Get<T>(Guid id)
 		{
+			T entity = default(T);
+
 			string fileName = $"{typeof(T).GetType().Name}.{id.ToString()}.txt";
 			IFile file = await rootFolder.GetFileAsync(fileName);
-			string contents = await file.ReadAllTextAsync();
-			T entity = JsonConvert.DeserializeObject<T>(contents);
+
+			if (file != null)
+			{
+				string contents = await file.ReadAllTextAsync();
+				entity = JsonConvert.DeserializeObject<T>(contents);
+			}
+
 			return entity;
+		}
+
+		public async Task<IEnumerable<T>> GetAll<T>()
+		{
+			string prefix = $"{typeof(T).GetType().Name}";
+
+			var files = (await rootFolder.GetFilesAsync())
+				.Where(x => x.Name.StartsWith(prefix)).ToList();
+
+			var entities = new List<T>();
+
+			foreach (var file in files)
+			{
+				string contents = await file.ReadAllTextAsync();
+				T entity = JsonConvert.DeserializeObject<T>(contents);
+
+				if (entity != null)
+					entities.Add(entity);
+			}
+
+			return entities;
 		}
 	}
 }
