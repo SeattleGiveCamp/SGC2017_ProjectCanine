354d38a522198477d993e95f3c0eb4a948f0a357
Geoffrey Liu <geliu@groupon.com>
2017-10-08 08:33:57 -0700

    title binding property for long answer qc

--- src/ProjectCanine/Views/QuestionControls/LongAnswerQC.xaml.cs
+++ src/ProjectCanine/Views/QuestionControls/LongAnswerQC.xaml.cs
@@ -6,9 +6,18 @@ namespace ProjectCanine
 {
     public partial class LongAnswerQC : ContentView
     {
+        public static BindableProperty TitleProperty;
         public LongAnswerQC()
         {
             InitializeComponent();
+            TitleProperty = BindableProperty.Create("Title", typeof(String), typeof(LongAnswerQC), "Long answer label");
+            BindingContext = this;
         }
+
+		public string Title
+		{
+			get { return (string)GetValue(TitleProperty); }
+			set { SetValue(TitleProperty, value); }
+		}
     }
 }
