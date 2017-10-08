6147541ac20af73650d2ed5bac089b63314ec378
Alina-Liokumovich <alinalkm87@gmail.com>
2017-10-08 09:55:51 -0700

    Fix datastore

--- src/ProjectCanine/ViewModels/TestSectionViewModel.cs
+++ src/ProjectCanine/ViewModels/TestSectionViewModel.cs
@@ -37,7 +37,7 @@ namespace ProjectCanine
 			try
 			{
 				Items.Clear();
-				var items = await DataStore.GetItemsAsync(true);
+				var items = await DataStore.GetItemsAsync();
 				foreach (var item in items)
 				{
 					Items.Add(item);
