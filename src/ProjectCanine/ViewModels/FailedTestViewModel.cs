bce180acf129b9c71d488c86f511d9f6ae0066c9
Alina-Liokumovich <alinalkm87@gmail.com>
2017-10-07 22:55:58 -0700

    database info for test one

---
+++ src/ProjectCanine/ViewModels/FailedTestViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ProjectCanine.ViewModels
+{
+    public class FailedTestViewModel : ContentPage
+    {
+        public FailedTestViewModel()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ src/ProjectCanine/Views/FailedTestPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ProjectCanine.Views
+{
+    public partial class FailedTestPage : ContentPage
+    {
+        public FailedTestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
