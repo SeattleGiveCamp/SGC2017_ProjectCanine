c58f69ecbe28f67a2300bb39e09599844b484c10
Marcel Englmaier <saurutobi@gmail.com>
2017-11-11 14:12:38 -0800

    added sections and questions display for the tests

---
+++ src/ProjectCanine.AdminPortal/ViewModels/QuestionGridRow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectCanine.AdminPortal.ViewModels
+{
+	public class QuestionGridRow
+	{
+		public Guid Id { get; set; }
+
+		[Display(Name = "Text")]
+		public String Text { get; set; }
+
+		[Display(Name = "MustPass")]
+		public bool MustPass { get; set; }
+
+		[Display(Name = "QuestionNumber")]
+		public int QuestionNumber { get; set; }
+		
+		[Display(Name = "HasYesNo")]
+		public bool HasYesNo { get; set; }
+
+		[Display(Name = "Last Edited By")]
+		public String LastEditedBy { get; set; }
+
+		[Display(Name = "Last Edited Date")]
+		public DateTime LastEditedDate { get; set; }
+	}
+}
