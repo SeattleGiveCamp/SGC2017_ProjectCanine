81d725015fb8351dffa5c2777f2004f54a02e1c3
Marcel Englmaier <saurutobi@gmail.com>
2017-12-26 10:30:29 +0100

    Renamed files to AdminPortal 1

--- src/ProjectCanine.AdminPortal/Models/ProjectCanineDB.Designer.cs
+++ src/ProjectCanine.AdminPortal/Models/ProjectCanineDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Source\SGC2017_ProjectCanine\src\ProjectCanine.AdminPortal3\Models\ProjectCanineDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Source\SGC2017_ProjectCanine\src\ProjectCanine.AdminPortal\Models\ProjectCanineDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
